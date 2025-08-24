c0f64c0708793fcc3867145257814a736b0ba469
Abubakkar Siddique <181467043+AbubakkarSiddique1930@users.noreply.github.com>
2025-08-24 11:32:08 +0600

    add: basic cmake config and hello world code

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <ios>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
