17351f11acbb23e66b0bf91d95f76727340d64dd
Abubakkar Siddique <181467043+AbubakkarSiddique1930@users.noreply.github.com>
2025-08-26 16:34:59 +0600

    feat: basic skia rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <GLFW/glfw3.h>
+#include <include/core/SkColor.h>
+#include <include/core/SkPaint.h>
 #include <ios>
 #include <iostream>
 #include <stdexcept>
 #include <spdlog/spdlog.h>
 
 #include <window.hpp>
+#include <sk.hpp>
 
 int main(int argc, char const *argv[]) {
     std::ios_base::sync_with_stdio(false);
@@ -14,19 +17,29 @@ int main(int argc, char const *argv[]) {
 
     try {
         InitGLFW();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         mainWindow = Window(800, 600, "gitxplorer");
+        SK::Init(800, 600);
     } catch(const std::runtime_error& err) {
         spdlog::error(err.what());
         return -1;
     }
 
+    SkPaint bgPaint;
+    bgPaint.setColor(SkColors::kCyan);
+
     while(mainWindow.ShouldNotClose()) {
         glfwPollEvents();
         if(glfwGetKey(mainWindow, GLFW_KEY_ESCAPE))
             break;
+
+        SK::canvas->drawPaint(bgPaint);
+
+        SK::context->flush();
+
+        glfwSwapBuffers(mainWindow);
     }
 
+    SK::Terminate();
     mainWindow.Terminate();
     TerminateGLFW();
 
---
+++ src/sk.cpp
@@ -0,0 +1,57 @@
+#include <sk.hpp>
+
+#include <GLFW/glfw3.h>
+#include <include/gpu/ganesh/GrBackendSurface.h>
+#include <include/gpu/ganesh/SkSurfaceGanesh.h>
+#include <include/gpu/ganesh/gl/GrGLAssembleInterface.h>
+#include <include/gpu/ganesh/gl/GrGLBackendSurface.h>
+#include <include/gpu/ganesh/gl/GrGLDirectContext.h>
+#include <include/gpu/ganesh/gl/GrGLInterface.h>
+#include <include/core/SkColorSpace.h>
+
+GrDirectContext* SK::context = nullptr;
+SkSurface* SK::surface = nullptr;
+SkCanvas* SK::canvas = nullptr;
+
+void SK::Init(int w, int h) {
+    auto interface = GrGLMakeNativeInterface();
+    if(!interface)
+        interface = GrGLMakeAssembledInterface(
+            nullptr,
+            (GrGLGetProc)* [](void*, const char* p) -> void* {
+                return (void*)glfwGetProcAddress(p);
+            }
+        );
+
+    context = GrDirectContexts::MakeGL(interface).release();
+
+    GrGLFramebufferInfo fbInfo;
+    fbInfo.fFBOID = 0;
+    fbInfo.fFormat= GL_RGBA8;
+
+    SkColorType colType = kRGBA_8888_SkColorType;
+    GrBackendRenderTarget rndrTrgt = GrBackendRenderTargets::MakeGL(
+        w, h, 
+        0,
+        0,
+        fbInfo
+    );
+
+    surface = SkSurfaces::WrapBackendRenderTarget(
+        context,
+        rndrTrgt,
+        kBottomLeft_GrSurfaceOrigin,
+        colType,
+        nullptr, nullptr
+    ).release();
+    if(!surface)
+        throw std::runtime_error("Failed to create Skia surface");
+
+    canvas = surface->getCanvas();
+}
+
+void SK::Terminate()
+{
+    delete surface;
+    delete context;
+}
