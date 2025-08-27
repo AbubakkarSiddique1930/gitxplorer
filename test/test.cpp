ba774189ebda72829277420a7ace464b047ff2c9
Abubakkar Siddique <181467043+AbubakkarSiddique1930@users.noreply.github.com>
2025-08-27 18:13:38 +0600

    add(test): test app

---
+++ test/test.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdio>
+#include <include/core/SkCanvas.h>
+#include <include/core/SkColor.h>
+#include <include/core/SkData.h>
+#include <include/core/SkImage.h>
+#include <include/core/SkImageInfo.h>
+#include <include/core/SkPaint.h>
+#include <include/core/SkRefCnt.h>
+#include <include/core/SkSurface.h>
+#include <include/core/SkStream.h>
+#include <include/encode/SkPngEncoder.h>
+
+#include <font.hpp>
+
+void WriteSurfaceToPNG(sk_sp<SkSurface> surface, const char* imagePath) {
+    sk_sp<SkImage> img = surface->makeImageSnapshot();
+
+    auto png = SkPngEncoder::Encode(nullptr, img.get(), {});
+    SkFILEWStream file(imagePath);
+    file.write(png->data(), png->size());
+}
+
+int main(int argc, char* argv[]) {
+    
+    if(argc < 2)
+        return -1;
+
+    SkImageInfo iinfo = SkImageInfo::MakeN32Premul(256, 256);
+    auto surface = SkSurfaces::Raster(iinfo);
+    SkCanvas* canvas = surface->getCanvas();
+
+    SkPaint bgPaint{SkColors::kCyan};
+    SkPaint fgPaint{SkColors::kBlack};
+
+    SkFont fontMononoki = Font::LoadFontFromFile(
+        "resources/fonts/mononoki/mononoki-Regular.ttf", 32
+    );
+    SkFont fontComfortaa = Font::LoadFontFromFile(
+        "resources/fonts/Comfortaa/Comfortaa-Medium.ttf", 32
+    );
+
+    canvas->drawPaint(bgPaint);
+    canvas->drawSimpleText(
+        "Hello", 5, SkTextEncoding::kUTF8,
+        50, 50, fontMononoki, fgPaint
+    );
+    canvas->drawSimpleText(
+        "World", 5, SkTextEncoding::kUTF8,
+        50, 90, fontComfortaa, fgPaint
+    );
+
+    WriteSurfaceToPNG(surface, argv[1]);
+
+    return 0;
+}
