3781ac71b869326ac41fced366a081c06f6889ba
Abubakkar Siddique <181467043+AbubakkarSiddique1930@users.noreply.github.com>
2025-08-27 21:23:58 +0600

    fix: separate MacOS from other UNIX

--- src/font.cpp
+++ src/font.cpp
@@ -4,19 +4,19 @@
 #include <include/core/SkTypeface.h>
 
 #ifdef __unix__
-    #include <include/ports/SkFontMgr_fontconfig.h>
-    sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_FontConfig(nullptr);
-#endif
-
-#ifdef __APPLE__
-    #include <include/ports/SkFontMgr_mac_ct.h>
-    sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_CoreText(nullptr);
-#endif
+    #ifdef __APPLE__
+        #include <include/ports/SkFontMgr_mac_ct.h>
+        sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_CoreText(nullptr);
+    #else // Other UNIX devices
+        #include <include/ports/SkFontMgr_fontconfig.h>
+        sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_FontConfig(nullptr);
+   #endif // __APPLE__ & other UNIX devices
+#endif // __unix__
 
 #ifdef _WIN32
     #include <include/ports/SkTypeface_win.h>
     sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_DirectWrite(nullptr);
-#endif
+#endif // _WIN32
 
 sk_sp<SkTypeface> Font::LoadTypeFaceFromFile(const char *filePath) {
     return fontMgr->makeFromFile(filePath);
