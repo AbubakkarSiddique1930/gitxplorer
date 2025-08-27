08c08aed75993f5615ddd738ac6279f83f2efb97
Abubakkar Siddique <181467043+AbubakkarSiddique1930@users.noreply.github.com>
2025-08-28 00:00:25 +0600

    fix: use proper style and syntax

--- src/font.cpp
+++ src/font.cpp
@@ -3,20 +3,19 @@
 #include <include/core/SkFontMgr.h>
 #include <include/core/SkTypeface.h>
 
-#ifdef __unix__
-    #ifdef __APPLE__
-        #include <include/ports/SkFontMgr_mac_ct.h>
-        sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_CoreText(nullptr);
-    #else // Other UNIX devices
-        #include <include/ports/SkFontMgr_fontconfig.h>
-        sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_FontConfig(nullptr);
-   #endif // __APPLE__ & other UNIX devices
-#endif // __unix__
-
-#ifdef _WIN32
+// Platform dependent FontMgr
+#ifdef __APPLE__
+    #include <include/ports/SkFontMgr_mac_ct.h>
+    sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_CoreText(nullptr);
+#elif __unix__
+    #include <include/ports/SkFontMgr_fontconfig.h>
+    sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_FontConfig(nullptr);
+#elif __WIN32
     #include <include/ports/SkTypeface_win.h>
     sk_sp<SkFontMgr> fontMgr = SkFontMgr_New_DirectWrite(nullptr);
-#endif // _WIN32
+#else
+    #error Platform not supported for FontMgr
+#endif // Platform dependent FontMgr
 
 sk_sp<SkTypeface> Font::LoadTypeFaceFromFile(const char *filePath) {
     return fontMgr->makeFromFile(filePath);
